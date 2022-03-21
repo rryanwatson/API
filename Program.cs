e5435e10c8ab558c5f2b829cdb18c70ac3a18122
Ryan <rryanwatson@users.noreply.github.com>
2022-03-21 11:41:35 -0600

    remove some comments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 
-//cd API
-//dotnet user-secrets set "ConnectionStrings:Default" "Server=tcp:interview-tests.database.windows.net,1433;Initial Catalog=test;Persist Security Info=False;User ID=interviewee;Password=8qCRg5EL38cpgeph;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
-//cd Client
-//npm i
-//npm run startDev
-
 //see: https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-6.0 for default configs
 var builder = WebApplication.CreateBuilder(args);
 
